a93ff9cd94ebef12461bda37323825b67dd2629f
Liang Zhang <liang.zhang-10@student.manchester.ac.uk>
2024-03-16 21:34:31 +0000

    Initial Mona bot solution using hard-coded values

--- Arduino_Files/Mona/Mona.ino
+++ Arduino_Files/Mona/Mona.ino
@@ -22,20 +22,33 @@ const int RIGHT_MOTOR_SPEED = 9;
 const int LEFT_SPEED = 111;
 const int RIGHT_SPEED = 111;
 
-const int IR_THRESHOLD = 975;
+const int IR_THRESHOLD = 1000;
 
 const int LOOP_DELAY = 100;
 
-// moving state of Mona tobot
-enum State {
-  movingLeft,
-  movingRight,
-  none
-};
-State state = none;
+// ~2.65s for 1 full 360 degrees rotation (at 111 speed)
+const int TIME_FOR_FULL_ROTATION = 1210;
 
-// TODO: delete later
-// int counter = 0;
+// ~1.75s for moving forwards 1 full block (at 111 speed)
+const int TIME_FOR_FULL_BLOCK = 1600;
+
+enum State {
+  turningLeft,
+  turningRight,
+  movingForward,
+  normal
+};
+// whether Mona robot is currently partway through turning, moving or neither
+State state = normal;
+
+// 0 degrees is original orientation
+// negative means left of original orientation
+// positive means right of original orientation
+double bearing = 0;
+double wantedBearing = 0;
+
+// counter for moving forwards 1 block
+int counter = 0;
 
 void irRead() {
   // IR enable
@@ -48,11 +61,11 @@ void irRead() {
 
   // take NUM_OF_READINGS IR readings over a period of time and then average them
   for (int i = 0; i < NUM_OF_READINGS; i++) {
-    irRight += analogRead(A3);
-    irRightFront += analogRead(A2);
+    irLeft += analogRead(A3);
+    irLeftFront += analogRead(A2);
     irFront += analogRead(A1);
-    irLeftFront += analogRead(A0);
-    irLeft += analogRead(A7);
+    irRightFront += analogRead(A0);
+    irRight += analogRead(A7);
     delay(20);
   }
 
@@ -64,30 +77,30 @@ void irRead() {
 }
 
 void forward() {
-  digitalWrite(LEFT_MOTOR_DIRECTION, FORWARD);
+  digitalWrite(LEFT_MOTOR_DIRECTION, BACKWARD);
   analogWrite(LEFT_MOTOR_SPEED, LEFT_SPEED);
-  digitalWrite(RIGHT_MOTOR_DIRECTION, FORWARD);
+  digitalWrite(RIGHT_MOTOR_DIRECTION, BACKWARD);
   analogWrite(RIGHT_MOTOR_SPEED, RIGHT_SPEED);
 }
 
 void backward() {
-  digitalWrite(LEFT_MOTOR_DIRECTION, BACKWARD);
+  digitalWrite(LEFT_MOTOR_DIRECTION, FORWARD);
   analogWrite(LEFT_MOTOR_SPEED, LEFT_SPEED);
-  digitalWrite(RIGHT_MOTOR_DIRECTION, BACKWARD);
+  digitalWrite(RIGHT_MOTOR_DIRECTION, FORWARD);
   analogWrite(RIGHT_MOTOR_SPEED, RIGHT_SPEED);
 }
 
 void left() {
-  digitalWrite(LEFT_MOTOR_DIRECTION, FORWARD);
+  digitalWrite(LEFT_MOTOR_DIRECTION, BACKWARD);
   analogWrite(LEFT_MOTOR_SPEED, LEFT_SPEED);
-  digitalWrite(RIGHT_MOTOR_DIRECTION, BACKWARD);
+  digitalWrite(RIGHT_MOTOR_DIRECTION, FORWARD);
   analogWrite(RIGHT_MOTOR_SPEED, RIGHT_SPEED);
 }
 
 void right() {
-  digitalWrite(LEFT_MOTOR_DIRECTION, BACKWARD);
+  digitalWrite(LEFT_MOTOR_DIRECTION, FORWARD);
   analogWrite(LEFT_MOTOR_SPEED, LEFT_SPEED);
-  digitalWrite(RIGHT_MOTOR_DIRECTION, FORWARD);
+  digitalWrite(RIGHT_MOTOR_DIRECTION, BACKWARD);
   analogWrite(RIGHT_MOTOR_SPEED, RIGHT_SPEED);
 }
 
@@ -97,6 +110,81 @@ void stop() {
   analogWrite(LEFT_MOTOR_SPEED, 0);
   digitalWrite(RIGHT_MOTOR_DIRECTION, FORWARD);
   analogWrite(RIGHT_MOTOR_SPEED, 0);
+}
+
+void move() {
+  // if the robot is already moving left, then finish moving left
+  if (state != normal) {
+    handleState();
+  } else {
+    // move left if possible
+    // otherwise, move forward if possible
+    // otherwise, move right
+    if (irLeft > IR_THRESHOLD) {
+      state = turningLeft;
+      wantedBearing = bearing - 90;
+      handleState();
+    } else if (irFront > IR_THRESHOLD) {
+      state = movingForward;
+      handleState();
+    } else {
+      state = turningRight;
+      wantedBearing = bearing + 90;
+      handleState();
+    }
+  }
+}
+
+void handleState() {
+  if (state == turningLeft) {
+    left();
+    bearing -= 360.0 / TIME_FOR_FULL_ROTATION * LOOP_DELAY;
+    // already turned left past wanted bearing
+    Serial.print("Bearing: ");
+    Serial.println(bearing);
+    if (wantedBearing >= bearing) {
+      state = movingForward;
+      bearing = 0;
+    }
+  } else if (state == turningRight) {
+    right();
+    bearing += 360.0 / TIME_FOR_FULL_ROTATION * LOOP_DELAY;
+    // already turned right past wanted bearing
+    if (wantedBearing <= bearing) {
+      state = movingForward;
+      bearing = 0;
+    }
+  } else if (state == movingForward) {
+    forward();
+    counter += LOOP_DELAY;
+    // already gone past 1 block
+    if (counter >= TIME_FOR_FULL_BLOCK) {
+      state = normal;
+      counter = 0;
+    }
+  }
+}
+
+// for debugging purposes
+void printInfo() {
+  // Serial.println("----------------------");
+  // Serial.print("Left IR reading: ");
+  // Serial.println(irLeft);
+  // Serial.print("Front Left IR reading: ");
+  // Serial.println(irLeftFront);
+  // Serial.print("Front IR reading: ");
+  // Serial.println(irFront);
+  // Serial.print("Front Right IR reading: ");
+  // Serial.println(irRightFront);
+  // Serial.print("Right IR reading: ");
+  // Serial.println(irRight);
+  Serial.print("State: ");
+  Serial.println(state);
+  // Serial.print("Bearing: ");
+  // Serial.println(bearing);
+  // Serial.print("Wanted bearing: ");
+  // Serial.println(wantedBearing);
+  // Serial.println("----------------------");
 }
 
 void setup() {
@@ -111,50 +199,11 @@ void setup() {
   forward();
 }
 
-// void temp() {
-//   if (counter < 2) {
-//     forward();
-//   } else if (counter < 4) {
-//     left();
-//   } else if (counter < 6) {
-//     backward();
-//   } else if (counter < 8) {
-//     right();
-//   } else if (counter < 10) {
-//     stop();
-//   } else {
-//     counter = -1;
-//   }
-//   counter++;
-// }
-
-void move() {
-  // if the robot is already moving left or right, then finish moving left or right 90 degrees
-  if (state == movingLeft) {
-    left();
-  } else if (state == movingRight) {
-    right();
-  } else {
-    // move left if possible
-    // otherwise, move forward if possible
-    // otherwise, move right
-    if (irLeft > IR_THRESHOLD) {
-      state = movingLeft;
-      left();
-    } else if (irFront > IR_THRESHOLD) {
-      forward();
-    } else {
-      state = movingRight;
-      right();
-    }
-  }
-}
-
 void loop() {
   digitalWrite(TOP_LED, HIGH);
   irRead();
-  // temp();
-  forward();
+  move();
+  printInfo();
   digitalWrite(TOP_LED, LOW);
   delay(LOOP_DELAY);
 }
